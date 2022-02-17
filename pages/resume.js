2c3f5044b96777eef9f71a0f0481814124b11efc
vishwajeetsharma <vishwajeetsharma9821593401@gmail.com>
2022-02-17 10:14:27 +0530

    resume bug fixing test commit

--- pages/resume.js
+++ pages/resume.js
@@ -11,7 +11,7 @@ export default function Home() {
           <h3>Vishwajeet Sharma</h3>
       </section>
       <section align="center">
-      <iframe src="https://onedrive.live.com/embed?cid=301D1E744BB92512&amp;resid=301D1E744BB92512%2116082&amp;authkey=ALAhu9Mgj1TnOHM&amp;em=2" width="800px" height="650px" frameborder="0">This is an embedded <a target="_blank" href="https://office.com">Microsoft Office</a> document, powered by <a target="_blank" href="https://office.com/webapps">Office</a>.</iframe>
+      <iframe src="/resume.html"  width="100%"></iframe>
       </section>
       <section className="back-to-home">
       <div style={{padding:"15px 40px", marginTop:"15px", marginLeft:"100px"}}>
