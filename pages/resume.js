266b42009f5f288f2a54a145a3f581ba68377968
vishwajeetsharma <vishwajeetsharma9821593401@gmail.com>
2022-02-18 12:31:14 +0530

    testing again

--- pages/resume.js
+++ pages/resume.js
@@ -1,25 +1,12 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Router from "next/router"
+import { useEffect } from "react"
 export default function Home() {
+  useEffect(() =>{
+    Router.push('/resume.html')
+  })
   return (
     <>
-      <Head>
-        <title>Resume | Vishwajeet Sharma</title>
-      </Head>
-      <section align="center" className="heading">
-          <h1>Resume</h1>
-          <h3>Vishwajeet Sharma</h3>
-      </section>
-      <section align="center">
-      <iframe src="/resume.html"  width="100%"></iframe>
-      </section>
-      <section className="back-to-home">
-      <div style={{padding:"15px 40px", marginTop:"15px", marginLeft:"100px"}}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      </section>
+      This is something
     </>
   )
 }
